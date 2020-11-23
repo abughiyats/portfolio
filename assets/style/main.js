1f98570ba670c5150c2a5072343d93a09be8e22b
abughiyats <anggaprabes@gmail.com>
2020-11-23 07:21:38 +0700

    second commit

--- assets/style/main.js
+++ assets/style/main.js
@@ -50,8 +50,8 @@ sr.reveal('.skills__text',{});
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
 
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+/*SCROLL project*/
+sr.reveal('.project__img',{interval: 200}); 
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
